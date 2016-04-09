fca768d153d3cb59bb623fde642defc2f87086f8
Kathleen Fuh <kfuh@ghc26.ghc.andrew.cmu.edu>
2016-04-09 15:28:28 -0400

    start from scratch. basic struct and file creations

--- boruvka_sequential.cpp
+++ boruvka_sequential.cpp
@@ -1,24 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "mic.h"
 #include "graph.h"
-#include "graph_internal.h"
-#include "union_find.h"
 
-//return the minimum weight edge coming out of each vertex
-Edge* min_edges(Graph g){
-    int num_nodes = get_num_nodes(g);
-
-    for(int i = 0; i < num_nodes; i++){
-
-    }
-    return NULL;
-}
-
-Graph star_contract(Graph g){
-    return NULL;
-}
-
-Graph find_MST(Graph g){
-    return NULL;
+//modifies the graph g directly instead of returning something
+void find_MST(Graph g){
+    
 }
--- graph.h
+++ graph.h
@@ -1,7 +1,5 @@
-#ifndef __GRAPH_H__
-#define __GRAPH_H__
-
 typedef int Vertex;
+
 struct Edge {
     Vertex src;
     Vertex dest;
@@ -9,32 +7,18 @@ struct Edge {
 };
 typedef struct Edge Edge;
 
-struct ListNode {
-    Edge e;
-    ListNode* next;
+struct graph {
+    int num_nodes;
+    int num_edges;
+
+    Edge* edges;
 };
-typedef struct ListNode ListNode;
+typedef graph* Graph'
 
-typedef struct graph* Graph;
+int get_num_nodes(Graph g){
+    return g->num_nodes;
+}
 
-/* Getters */
-static inline int num_nodes(const Graph);
-static inline int num_edges(const Graph);
-
-
-/* IO  - I'm not sure where these are defined or if we need them */
-Graph load_graph(const char* filename);
-Graph load_graph_binary(const char* filename);
-void store_graph_binary(const char* filename, Graph);
-
-void print_graph(const Graph);
-
-
-/* Deallocation */
-void free_graph(Graph);
-
-
-/* Included here to enable inlining. Don't look. */
-#include "graph_internal.h"
-
-#endif
+int get_num_edges(Graph g){
+    return g->num_edges;
+}
