932dc39ed83c89612063035b8e656e6c5b0ac6dd
Kathleen Fuh <kfuh@ghc26.ghc.andrew.cmu.edu>
2016-04-12 17:19:49 -0400

    fix infinite loop - mst not correct

--- boruvka_parallel.h
+++ boruvka_parallel.h
@@ -4,4 +4,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void find_MST_parallel(Graph g){
+    int n = get_num_nodes(g);
+    //store the edge index of the min weight edge incident on node i
+    int min_edges[n];
+    struct set *components = new struct set[n];
+    int num_components = n;
+    
+    //initialize min_edges array
+    #pragma omp parallel for schedule(static)
+    for(int i = 0; i < n; i++){
+        components[i].parent = i;
+        components[i].rank = 0;
+        min_edges[i] = -1;
+    }
+
+    while(num_components > 1){
+        //find minimum weight edge out of each componenet
+        #pragma omp parallel for schedule(static)
+        for(int i = 0; i < n; i++){
+            ListNode edgeNode = g->adjList[i];
+            int curr_min_weight = 0;
+            while(edgeNode != NULL){
+                if(min_edges[i] == -1){
+                    min_edges[i] = edgeNode->e.dest;
+                    curr_min_weight = edgeNode->e.weight;
+                }
+                else{
+                    if(edgeNode->e.weight < curr_min_weight){
+                        min_edges[i] = edgeNode->e.dest;
+                        curr_min_weight = edgeNode->e.weight;
+                    }
+                }
+                edgeNode = edgeNode->next;
+            }
+        }
+        //contract based on min edges found
+        for(int i = 0; i < n; i++){
+            int dest = min_edges[i];
+            int root1 = find(components, i);
+            int root2 = find(components, dest);
+            if(root1 == root2){
+                continue;
+            }
+            union_sets(components, i, dest);
+            num_components--;
+        }
+    }
+   
+    for(int i = 0; i < n; i++) {
+      if(min_edges[min_edges[i]] == i) {
+        min_edges[i] = -1;
+      }
+    }
+
+    for(int i = 0; i < n; i++){
+      if (min_edges[i] != -1)
+        printf("src %d to dest %d\n", i, min_edges[i]);
+    }
+}
 #endif
--- boruvka_sequential.h
+++ boruvka_sequential.h
@@ -10,39 +10,47 @@
 void find_MST(Graph g){
     int n = get_num_nodes(g);
     //store the edge index of the min weight edge incident on node i
-    int min_edges[n];
+    struct Edge* min_edges = new struct Edge[n];
     struct set *components = new struct set[n];
     int num_components = n;
-    
+   
+    /*Edge emptyEdge;
+    emptyEdge.src = -1;
+    emptyEdge.dest = -1;
+    emptyEdge.weight = -1; */
     //initialize min_edges array
     for(int i = 0; i < n; i++){
         components[i].parent = i;
         components[i].rank = 0;
-        min_edges[i] = -1;
+        //min_edges[i] = emptyEdge;
     }
 
     while(num_components > 1){
         //find minimum weight edge out of each componenet
         for(int i = 0; i < n; i++){
             ListNode edgeNode = g->adjList[i];
-            int curr_min_weight = 0;
+            bool isFirstPass = true;
+            //int curr_min_weight = 0;
             while(edgeNode != NULL){
-                if(min_edges[i] == -1){
-                    min_edges[i] = edgeNode->e.dest;
-                    curr_min_weight = edgeNode->e.weight;
-                }
-                else{
-                    if(edgeNode->e.weight < curr_min_weight){
-                        min_edges[i] = edgeNode->e.dest;
-                        curr_min_weight = edgeNode->e.weight;
+        //        printf("hello");
+                //get representative nodes
+                int set1 = find(components, i);
+                int set2 = find(components, edgeNode->e.dest);
+                //this edge has already been contracted (endpoints in same component)
+                if(set1 != set2){
+                    if(isFirstPass){
+                        min_edges[set1] = edgeNode->e; 
+                        isFirstPass = false;
                     }
+                    else if (min_edges[set1].weight > edgeNode->e.weight)
+                        min_edges[set1] = edgeNode->e;
                 }
                 edgeNode = edgeNode->next;
             }
         }
         //contract based on min edges found
         for(int i = 0; i < n; i++){
-            int dest = min_edges[i];
+            int dest = min_edges[i].dest;
             int root1 = find(components, i);
             int root2 = find(components, dest);
             if(root1 == root2){
@@ -51,17 +59,18 @@ void find_MST(Graph g){
             union_sets(components, i, dest);
             num_components--;
         }
+        
     }
    
-    for(int i = 0; i < n; i++) {
+    /*for(int i = 0; i < n; i++) {
       if(min_edges[min_edges[i]] == i) {
         min_edges[i] = -1;
       }
-    }
+    }*/
 
     for(int i = 0; i < n; i++){
-      if (min_edges[i] != -1)
-        printf("src %d to dest %d\n", i, min_edges[i]);
+      if (min_edges[i].src != -1)
+        printf("src %d to dest %d\n", i, min_edges[i].dest);
     }
 }
 #endif
