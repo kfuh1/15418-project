0acd2b9c3376f34cf22d7354776d8df743e781b0
Shreya Vemuri <shreyav@unix3.andrew.cmu.edu>
2016-04-12 23:53:48 -0400

    create graph file

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 #include "boruvka_sequential.h"
 #include "graph.h"
 #include "union_find.h"
+#include "graph_tests.h"
 
-Graph createGraph(int V, int E)
+/**Graph createGraph(int V, int E)
 {
     Graph g = new graph;
     g->num_nodes = V;
     g->num_edges = E;
     g->adjList = new ListNode[2*E];
     return g;
-}
+}**/
 
 int main(int argc, char** argv){
-    int V = 9;  // Number of vertices in graph
-    int E = 14;  // Number of edges in graph
+    //int V = 4;  // Number of vertices in graph
+    //int E = 5;  // Number of edges in graph
     
-    Graph g = createGraph(V, E);
+    //Graph g = createGraph(V, E);
             
     // add edge 0-1
-    struct Edge e0;
+    /**struct Edge e0;
     e0.src = 0;
     e0.dest = 1;
     e0.weight = 4;
@@ -189,7 +190,68 @@ int main(int argc, char** argv){
     e13_rev.weight = 1;
     insert(&(g->adjList[e13_rev.src]), e13_rev);
 
-    for (int i = 0; i < V; i++) {
+    struct Edge e0;
+    e0.src = 0;
+    e0.dest = 1;
+    e0.weight = 10;
+    insert(&(g->adjList[e0.src]), e0);
+    struct Edge e0_rev;
+    e0_rev.src = 1;
+    e0_rev.dest = 0;
+    e0_rev.weight = 10;
+    insert(&(g->adjList[e0_rev.src]), e0_rev);
+    
+    // add edge 0-7
+    struct Edge e1;
+    e1.src = 0;
+    e1.dest = 2;
+    e1.weight = 6;
+    insert(&(g->adjList[e1.src]), e1);
+    struct Edge e1_rev;
+    e1_rev.src = 2;
+    e1_rev.dest = 0;
+    e1_rev.weight = 6;
+    insert(&(g->adjList[e1_rev.src]), e1_rev);
+                                  
+    // add edge 1-7
+    struct Edge e2;
+    e2.src = 0;
+    e2.dest = 3;
+    e2.weight = 5;
+    insert(&(g->adjList[e2.src]), e2);
+    struct Edge e2_rev;
+    e2_rev.src = 3;
+    e2_rev.dest = 0;
+    e2_rev.weight = 5;
+    insert(&(g->adjList[e2_rev.src]), e2_rev);
+                                                    
+    // add edge 7-8
+    struct Edge e3;
+    e3.src = 1;
+    e3.dest = 3;
+    e3.weight = 15;
+    insert(&(g->adjList[e3.src]), e3);
+    struct Edge e3_rev;
+    e3_rev.src = 3;
+    e3_rev.dest = 1;
+    e3_rev.weight = 15;
+    insert(&(g->adjList[e3_rev.src]), e3_rev);
+     
+    // add edge 1-2
+    struct Edge e4;
+    e4.src = 2;
+    e4.dest = 3;
+    e4.weight = 4;
+    insert(&(g->adjList[e4.src]), e4);
+    struct Edge e4_rev;
+    e4_rev.src = 3;
+    e4_rev.dest = 2;
+    e4_rev.weight = 4;
+    insert(&(g->adjList[e4_rev.src]), e4_rev);**/
+    
+    Graph g = createSevenCycleGraph();
+
+    for (int i = 0; i < 7; i++) {
        ListNode edgeNode = g->adjList[i];
        printf("Vertex %d ", i);
        while(edgeNode != NULL) {
