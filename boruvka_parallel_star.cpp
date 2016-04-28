481c2503f2612ddc5b77e0ef7d69c3edc4064b73
Kathleen Fuh <kfuh@andrew.cmu.edu>
2016-04-28 00:30:47 -0400

    initial setup for parallel star contraction implementation

---
+++ boruvka_parallel_star.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+#include "boruvka_parallel_star.h"
+#include "union_find.h"
+
+void find_MST_parallel_star(Graph g){
+}
